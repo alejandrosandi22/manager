b27f6f634fd3abc81226e4b36d98adacb4204653
alesandi22 <alesf22@gmail.com>
2022-02-10 13:46:12 -0600

    signup layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 import App from './App';
 import firebase from 'firebase/compat/app';
 import environment from './environments/environment'
