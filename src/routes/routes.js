7abfb4a99093af5384824403797c432e9e8ad384
alesandi22 <alesf22@gmail.com>
2022-02-13 16:43:18 -0600

    routes

---
+++ src/routes/routes.js
@@ -0,0 +1,57 @@
+import { useRoutes } from 'react-router';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import Dashboard from '../components/Dashboard/Dashboard';
+import Projects from '../components/Projects/Projects';
+import SignUp from '../components/SignUp/SignUp';
+import LogIn from '../components/LogIn/LogIn';
+import Recovery from '../components/Recovery/Recovery';
+
+const ProtectedRoutes = ({ children }) => {
+  
+  const navigate = useNavigate();
+  
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      return navigate('/login');
+    }
+  })
+  
+  return children;
+}
+
+const routes  = [
+  {
+    path: '/signup',
+    element: <SignUp />
+  },
+  {
+    path: '/login',
+    element: <LogIn />
+  },
+  {
+    path: '/recovery',
+    element: <Recovery />
+  },
+  {
+    path: '/',
+    element: <Navigate to='/dashboard'/>,
+  },
+  {
+    path: '/dashboard',
+    element: <ProtectedRoutes><Dashboard /></ProtectedRoutes>
+  },
+  {
+    path: '/projects',
+    element: <ProtectedRoutes><Projects /></ProtectedRoutes>
+  }
+]
+
+function Router() {
+  const element = useRoutes(routes);
+  return element;
+}
+
+export default Router;
--- src/shared/components/SignInButtons/SignInButtons.jsx
+++ src/shared/components/SignInButtons/SignInButtons.jsx
@@ -1,9 +1,26 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import './SignInButtons.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignInButtons() {
+  
+  const navigate = useNavigate();
+
+  const signInWithGoogle =  () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+     signInWithPopup(auth, provider).then(user => {
+      console.log(user)
+      navigate('/');
+    })
+
+
+  }
+
   return(
     <div className="sign-in-buttons">
-    <button className="google"><i className="fab fa-google"></i></button>
+    <button onClick={signInWithGoogle} className="google"><i className="fab fa-google"></i></button>
     <button className="facebook"><i className="fab fa-facebook-f"></i></button>
     <button className="github"><i className="fab fa-github"></i></button>
   </div>
